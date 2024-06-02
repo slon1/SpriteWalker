7eada5a4dbd8d99f86a2239f0d8d29173e41560f
Alexander <alex.emdin@gmial.com>
2024-06-02 14:01:53 +0200

    Grid model

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+
+namespace Model {
+	[Serializable]
+	public class Cell {
+		public int x;
+		public int y;
+		public bool isUnwalkable;
+
+		public Cell(int x, int y, bool isUnwalkable) {
+			this.x = x;
+			this.y = y;
+			this.isUnwalkable = isUnwalkable;
+		}
+	}
+}
